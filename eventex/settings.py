d291779480a626f883eba13bb453e45e8e94b472
Leonardo Augusto <sleonardoaugusto@gmail.com>
2020-09-21 00:48:18 -0300

    Fix static root

--- eventex/settings.py
+++ eventex/settings.py
@@ -113,4 +113,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = f"{BASE_DIR}/'staticfiles'"
+STATIC_ROOT = f"{BASE_DIR}/staticfiles"
