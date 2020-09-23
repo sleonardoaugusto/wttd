b274666417b07f54dbac391ead53a2abd3cf4038
Leonardo Augusto <sleonardoaugusto@gmail.com>
2020-09-23 01:26:57 -0300

    List filter added

--- eventex/subscriptions/admin.py
+++ eventex/subscriptions/admin.py
@@ -8,6 +8,7 @@ class SubscriptionModelAdmin(admin.ModelAdmin):
     list_display = ("name", "cpf", "email", "phone", "created_at", "subscribed_today")
     date_hierarchy = "created_at"
     search_fields = ("name", "cpf", "email", "phone", "created_at")
+    list_filter = ("created_at",)
 
     def subscribed_today(self, obj):
         return obj.created_at.date() == now().date()
