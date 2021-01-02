7a9ed5167a7ec7ee828456ff23595f008b2a0f5d
Leonardo Augusto <sleonardoaugusto@gmail.com>
2021-01-02 18:23:19 -0300

    ModelForm implemented

--- eventex/subscriptions/models.py
+++ eventex/subscriptions/models.py
@@ -1,13 +1,15 @@
 import uuid as uuid
 from django.db import models
 
+from eventex.subscriptions.validators import validate_cpf
+
 
 class Subscription(models.Model):
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100, verbose_name="nome")
-    cpf = models.CharField(max_length=11)
-    email = models.EmailField(verbose_name="e-mail")
-    phone = models.CharField(max_length=20, verbose_name="telefone")
+    cpf = models.CharField(max_length=11, validators=[validate_cpf])
+    email = models.EmailField(verbose_name="e-mail", blank=True)
+    phone = models.CharField(max_length=20, verbose_name="telefone", blank=True)
     paid = models.BooleanField(default=False, verbose_name="pago")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="criado em")
 
